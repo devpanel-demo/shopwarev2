8dc6a70d037e77b6d2b90b5ad92b6d38b0b49d5b
Boi Nguyen <boi@devpanel.com>
2025-08-14 16:17:40 +0700

    Update CI

--- .devpanel/custom_package_installer.sh
+++ .devpanel/custom_package_installer.sh
@@ -50,6 +50,7 @@ if [[ -f "$APP_ROOT/composer.json" ]]; then
   # cd $APP_ROOT && composer install;
   cd $APP_ROOT
   sudo mkdir -p vendor var public
+  sudo chown -R "$(whoami)":"$(whoami)" vendor/ var/ public/
   sudo chmod -R 775 vendor/ var/ public/
   composer install;
 
