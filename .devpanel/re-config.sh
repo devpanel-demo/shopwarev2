5796a7b59fe244b837f612708cad4400b12789a9
Boi Nguyen <boi@devpanel.com>
2025-08-14 11:28:12 +0700

    Update CI

--- .devpanel/re-config.sh
+++ .devpanel/re-config.sh
@@ -18,7 +18,8 @@
 # Update .env.local file
 CONNECT_STRING="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 if [ -n "$DP_HOSTNAME" ]; then
-    sed -i "s|APP_URL=http://127.0.0.1:8000|APP_URL=https://${DP_HOSTNAME}|" "$APP_ROOT/.env.local"
+    # sed -i "s|APP_URL=http://127.0.0.1:8000|APP_URL=https://${DP_HOSTNAME}|" "$APP_ROOT/.env.local"
+    echo "APP_URL=http://${DP_HOSTNAME}" >> "$APP_ROOT/.env.local"
 fi
 
 # Extract DB name from the connect string
@@ -27,7 +28,8 @@ DB_NAME=$(echo "$CONNECT_STRING" | cut -d'/' -f4)
 if mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME;" 2>/dev/null; then
     echo "Database '$DB_NAME' exists. Running Shopware install..."
     SAFE_CONNECT_STRING=$(printf '%s' "$CONNECT_STRING" | sed -e 's/[&|/$\\]/\\&/g')
-    sed -i "s|^DATABASE_URL=.*|DATABASE_URL=\"${SAFE_CONNECT_STRING}\"|" "$APP_ROOT/.env.local"
+    #sed -i "s|^DATABASE_URL=.*|DATABASE_URL=\"${SAFE_CONNECT_STRING}\"|" "$APP_ROOT/.env.local"
+    echo "\"${SAFE_CONNECT_STRING}\"" "$APP_ROOT/.env.local"
 
     echo '> Install shopware package';
     cd $APP_ROOT
