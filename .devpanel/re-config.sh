c200441405ff6a537062508590b9eebdc8c7ae7e
Boi Nguyen <boi@devpanel.com>
2025-08-19 10:20:42 +0700

    Test CI

--- .devpanel/re-config.sh
+++ .devpanel/re-config.sh
@@ -20,15 +20,23 @@ sudo mkdir -p vendor var public files
 sudo chown -R "$(whoami)":"$(whoami)" vendor/ var/ public/ files/ .env.local composer.json composer.lock
 sudo chmod -R 775 vendor/ var/ public/ files/ .env.local composer.json composer.lock
 
-if [ -f "$APP_ROOT/.env.local" ]; then
-  echo "✅ .env.local exists"
-else
-  echo "❌ .env.local does not exist"
-fi
-
+# URL-encode function (POSIX safe)
+urlencode() {
+    local raw="$1"
+    local length=${#raw}
+    local i char
+    for (( i = 0; i < length; i++ )); do
+        char="${raw:$i:1}"
+        case "$char" in
+            [a-zA-Z0-9.~_-]) printf '%s' "$char" ;;
+            *) printf '%%%02X' "'$char" ;;
+        esac
+    done
+}
+ENCODED_PASS=$(urlencode "$DB_PASSWORD")
 echo '> Update .env file'
 # Update .env.local file
-CONNECT_STRING="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+CONNECT_STRING="${DB_DRIVER}://${DB_USER}:${ENCODED_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 if [ -n "$DP_HOSTNAME" ]; then
   if ! grep -q '^APP_URL=' "$APP_ROOT/.env.local"; then
     echo "APP_URL=http://${DP_HOSTNAME}" >> "$APP_ROOT/.env.local"
@@ -86,11 +94,11 @@ if mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAM
     # composer require --dev shopware/dev-tools
 
     echo "> Import database"
-    # APP_ENV=prod bin/console framework:demodata && APP_ENV=prod bin/console dal:refresh:index
-    if [[ -f "$APP_ROOT/.devpanel/dumps/shopware.sql" ]]; then
-      echo  'Extract mysql files ...'
-      mysql -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME < $APP_ROOT/.devpanel/dumps/shopware.sql
-    fi
+    APP_ENV=prod bin/console framework:demodata && APP_ENV=prod bin/console dal:refresh:index
+    # if [[ -f "$APP_ROOT/.devpanel/dumps/shopware.sql" ]]; then
+    #   echo  'Extract mysql files ...'
+    #   mysql -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME < $APP_ROOT/.devpanel/dumps/shopware.sql
+    # fi
 
     bin/console cache:clear
 else
