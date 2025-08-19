92fd5cb0282d5c16e17c20823b249c1729a1185f
Boi Nguyen <boi@devpanel.com>
2025-08-20 06:44:33 +0700

    Test CI

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -16,6 +16,9 @@
 # ----------------------------------------------------------------------
 
 cd $APP_ROOT
+sudo mkdir -p vendor var public files
+sudo chown -R "$(whoami)":"$(whoami)" vendor/ var/ public/ files/ .env.local composer.json composer.lock
+sudo chmod -R 775 vendor/ var/ public/ files/ .env.local composer.json composer.lock
 
 # URL-encode function (POSIX safe)
 urlencode() {
@@ -34,24 +37,43 @@ ENCODED_PASS=$(urlencode "$DB_PASSWORD")
 echo '> Update .env file'
 # Update .env.local file
 CONNECT_STRING="${DB_DRIVER}://${DB_USER}:${ENCODED_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+if [ -n "$DP_HOSTNAME" ]; then
+  if ! grep -q '^APP_URL=' "$APP_ROOT/.env.local"; then
+    echo "APP_URL=http://${DP_HOSTNAME}" >> "$APP_ROOT/.env.local"
+  fi
+fi
 
 # Check if database exists
 if mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME;" 2>/dev/null; then
     echo "Database '$DB_NAME' exists. Running Shopware install..."
-
-    # Escape special chars for sed
-    SAFE_CONNECT_STRING=$(printf '%s' "$CONNECT_STRING" | sed -e 's/[&/\]/\\&/g')
-
+    SAFE_CONNECT_STRING=$(printf '%s' "$CONNECT_STRING" | sed -e 's/[&|/$\\]/\\&/g')
     if grep -q '^DATABASE_URL=' "$APP_ROOT/.env.local"; then
       sed -i "s|^DATABASE_URL=.*|DATABASE_URL=\"${SAFE_CONNECT_STRING}\"|" "$APP_ROOT/.env.local"
     else
       echo "DATABASE_URL=\"${CONNECT_STRING}\"" >> "$APP_ROOT/.env.local"
     fi
 
+    echo '> Install shopware package';
+    sudo bin/console system:install --basic-setup --force
+
+    sudo chown -R www-data:www-data var/ public/
+    sudo chmod -R 775 var/ public/
+    bin/console cache:clear
+
+    # Allow composer plugin without prompt
+    echo '> allow-plugins'
+    composer config --no-plugins allow-plugins.php-http/discovery true
+
+    # Install profiler and other dev tools, eg Faker for demo data generation
+    echo '> Install dev-tools'
+    composer require --dev shopware/dev-tools
+
     echo "> Import database"
     APP_ENV=prod bin/console framework:demodata && APP_ENV=prod bin/console dal:refresh:index
 
     bin/console cache:clear
+
+    echo "> Successful, please refresh your web page."
 else
     echo "Database '$DB_NAME' does not exist. Skipping install."
 fi
